b83700381593ad74f2a32bd3aaee04b5133e1ef5
Ophélie <roux.ophelie.or@gmail.com>
2024-01-26 11:42:20 +0100

    FrontEnd2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('whatyearisit-backend-fr5uv8q6h-ophelies-projects-722a4a6d.vercel.app')
+fetch(`whatyearisit-backend-fr5uv8q6h-ophelies-projects-722a4a6d.vercel.app`)
     .then(response => response.json())
     .then(data => {
       document.querySelector('#year').textContent = data.year;
